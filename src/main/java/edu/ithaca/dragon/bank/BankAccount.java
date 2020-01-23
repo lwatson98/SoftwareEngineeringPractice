4df4b679be257702862d05778cead35020a38aaf
Eugene Kuznetsov <Azurekuz@gmail.com>
2020-01-22 22:53:49 -0500

    Modified email validation

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -36,12 +36,39 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
+        String acceptableChar = "abcdefghijklmnopqrstuvwxyz_.-";
+        String[] validDomains = {".com",".org", ".cc"};
         if (email.indexOf('@') == -1){
             return false;
-        }
-        else {
+        }else if (!checkEmailValidChar(email, acceptableChar)) {
+            return false;
+        }else if(email.indexOf('.') == -1){
+            return false;
+        }else if(!checkAdjacentNonAlphaChar(email, acceptableChar)){
+            return false;
+        }else{
             return true;
         }
 
+    }
+
+    public static boolean checkEmailValidChar(String email, String acceptableChar){
+        for(int i = 0; i < email.length(); i++){
+            if(email.charAt(i) != '@'  && acceptableChar.indexOf(email.toLowerCase().charAt(i)) == -1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkAdjacentNonAlphaChar(String email, String acceptableChar){
+        for(int i = 0; i < email.length(); i++){
+            if(email.charAt(i) != '@'  && (i!=email.length()-1 && i!=0) && acceptableChar.substring(26,29).indexOf(email.toLowerCase().charAt(i)) != -1
+            && (acceptableChar.substring(0,26).indexOf(email.toLowerCase().charAt(i-1)) == -1 ||
+                    acceptableChar.substring(0,26).indexOf(email.toLowerCase().charAt(i+1)) == -1)){
+                return false;
+            }
+        }
+        return true;
     }
 }
